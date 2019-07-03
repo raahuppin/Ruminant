e91aba83a2509b88502407480d1dca7d653ed113
Sven Rappo <sven.rappo@students.gymneufeld.ch>
2019-07-03 10:56:17 +0200

    Flask 2

--- Ruminant/src/rumimant.py
+++ Ruminant/src/rumimant.py
@@ -1,36 +1,59 @@
 from flask import Flask, render_template, request, url_for, redirect, session
 from flask_mysqldb import MySQL
+
+import sys
+
+
 app=Flask(__name__)
+
+app.secret_key="wdjiwjdiwjd"
+
+app.config['MYSQL_USER'] = 'root'
+
+app.config['MYSQL_DB'] = 'datenbank_eshop'
+app.config['MYSQL_HOST'] = 'localhost'
 
 
 mysql = MySQL(app)
 @app.route("/")
 def eShop():
-    return redirect(url_for('home',))
+    return redirect(url_for('login',))
+
 @app.route("/home",methods=['GET','POST'])
 def Home():
-    return render_template('')
+    return render_template('home.html')
+
 @app.route("/checkout",methods=['GET','POST'])
 def Warenkorb():
     return("")
+
 @app.route("/shop",methods=['GET','POST'])
 def Waren():
-    return("")
+    return render_template('shop.html')
+
 @app.route("/login",methods=['GET','POST'])
 def login():
+    message=''
     if request.method=='POST':
         session['uname']=request.form['username']
         session['pword']=request.form['password']
         cursor = mysql.connection.cursor()
-        cursor.execute(’’’SELECT username,password FROM Login’’’)
-        logindata=cursor.fetchall()
+        cursor.execute('''SELECT passwort FROM Kunden where username like"'''+session.get('uname')+'''";''')
+        logindata=cursor.fetchone()
         mysql.connection.commit()
         cursor.close
-        if logindata(1)==session.get('uname') and logindata(2)==session.get('pword'):
-            loggedIn=true;
-            return redirect(url_for('Waren',))
+        
+        if logindata is not None:
+            for password in logindata:
+                
+                
+                if password==session['pword']:
+                    loggedIn="true"
+                    
+                    return redirect(url_for('Waren'))
     return render_template('Login.html',message=message)
 
 
+
 if __name__ == "__main__":
-   app.run()
+   app.run(debug=True)
